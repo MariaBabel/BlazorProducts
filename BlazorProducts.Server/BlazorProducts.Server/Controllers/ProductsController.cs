382b3a0050d4a4ed7032d5c557e639ab4c482419
María Babel <maria.ldominguez@babel.es>
2021-02-02 15:50:38 +0100

    First commit

---
+++ BlazorProducts.Server/BlazorProducts.Server/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using BlazorProducts.Server.Repository;
+using Entities.RequestFeatures;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorProducts.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _repo;
+        public ProductsController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] ProductParameters productParameters)
+        {
+            var products = await _repo.GetProducts(productParameters);
+            Response.Headers.Add("X-Pagination", JsonConvert.SerializeObject(products.MetaData));
+            return Ok(products);
+        }
+    }
+}
